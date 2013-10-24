c69039707f4d4c425a1436deaff449ca57cf3aad
Yilong Geng <gengyl08@stanford.edu>
2013-10-24 10:24:06 +0000

    first release

--- views/fuel.py
+++ views/fuel.py
@@ -44,20 +44,61 @@ def requires_liveuser_retain_path():
 
 @requires_liveuser_retain_path()
 def main(request, user):
-    return render_to_response(
-            'fuel/index.html',
-            {'user': user, 'consts':consts},
-            context_instance = RequestContext(request)
-            )
+    has_car = user.bundle(consts.CAPRI_FUEL).doAction(consts.CAPRI_FUEL_HAS_CAR)
+    if has_car:
+        return render_to_response(
+                'fuel/index.html',
+                {'user': user, 'consts':consts},
+                context_instance = RequestContext(request)
+                )
+    else:
+        return render_to_response(
+                'fuel/registration.html',
+                {'user': user, 'consts':consts},
+                context_instance = RequestContext(request)
+                )
 
+
+@csrf_protect
 @requires_liveuser
-def indextemp(request, user):
-    logging.getLogger('fe').debug('hi');
-    return render_to_response(
-            'fuel/index_temp.html',
-            {'user': user, 'consts':consts},
-            context_instance = RequestContext(request)
-            )
+def registration(request, user):
+    if request.method == 'POST':
+        try:
+            form = RegistrationForm.submit(request, user)
+        except FormExecuted:
+            return HttpResponse("success")
+    # if we got to this point: either the method was GET, or something wrong happened and the form was returned. 
+    return HttpResponse("error")
+
+
+
+
+class RegistrationForm(SequentialForm):
+    # define your fields here
+    make = forms.CharField(required=True)
+    model = forms.CharField(required=True)
+    year = forms.IntegerField(required=True, min_value=1970, max_value=2020)
+    @staticmethod
+    def submit(request, user):
+        # process your form here
+        form = RegistrationForm(request.POST)
+        if not form.is_valid():
+            logging.getLogger('fe').debug('not valid')
+            form.markErroneous('Invalid data submitted')
+            return form
+        else:
+            # process form, call API to update, etc
+            has_car = user.bundle(consts.CAPRI_FUEL).doAction(consts.CAPRI_FUEL_HAS_CAR)
+            if not has_car:
+                make = form.cleaned_data['make']
+                model = form.cleaned_data['model']
+                year = form.cleaned_data['year']
+                user.bundle(consts.CAPRI_FUEL).doAction(consts.CAPRI_FUEL_SET_CAR,
+                        car_data={'make':make, 'model':model, 'year':year})
+
+            raise FormExecuted()
+            
+
 
 @csrf_protect
 @requires_liveuser
@@ -70,40 +111,6 @@ def fuelup_post(request, user):
             return HttpResponse("success")
     # if we got to this point: either the method was GET, or something wrong happened and the form was returned. 
     return HttpResponse("error")
-
-
-# this view function implements the first step -- upload of image, typing in of information, and submission
-@csrf_protect
-@requires_liveuser
-def step1(request, user):
-
-    #return HttpResponse(request.POST['type']+request.FILES['image'])
-    # if there is a form to process, do it
-
-    if request.method == 'POST': 
-        # trigger form processing here
-        # sorry, this is a backwards design pattern in existing code. We're raising an exception to indicate that processing succeeded *(WHAT)*. Just try to flow with this...
-        try:
-            form = Step1Form.submit(request, user)
-
-        # form was successfully processed
-        except FormExecuted:
-            
-            d = {'user': user, 'consts': consts}
-            d.update(k.data)
-            # go onto the next step
-            return render_to_response(
-                    'fuel/secondstep.html',
-                    d,
-                    context_instance = RequestContext(request)
-                    )
-    
-    # if we got to this point: either the method was GET, or something wrong happened and the form was returned. 
-    return render_to_response(
-            'fuel/index.html',
-            {'user': user, 'consts':consts, 'form':Step1Form()},
-            context_instance = RequestContext(request)
-            )
 
 class FuelupForm(SequentialForm):
     # define your fields here
@@ -146,41 +153,6 @@ class FuelupForm(SequentialForm):
                     }
             raise k
 
-# this form handles the typing in of information and uploading of image
-class Step1Form(SequentialForm):
-    # define your fields here
-    image = forms.ImageField(required=True)
-    price = forms.FloatField(required=True,min_value=0,max_value=10.0)
-    gallons = forms.FloatField(required=True,min_value=0,max_value=30.0)
-#    type = forms.CharField(required=True)
-
-    @staticmethod
-    def submit(request, user):
-        # process your form here
-        form = Step1Form(request.POST, request.FILES)
-        if not form.is_valid():
-            logging.getLogger('fe').debug('not valid')
-            form.markErroneous('Invalid data submitted')
-            return form
-        else:
-            # process form, call API to update, etc
-            fuel_id = user.bundle(consts.CAPRI_FUEL).doAction(consts.CAPRI_FUEL_CREATE)
-            amount = float(form.cleaned_data['gallons'])
-            price = float(form.cleaned_data['price'])
-            logging.getLogger('fe').debug('%g %g', amount, price)
-            user.bundle(consts.CAPRI_FUEL).doAction(consts.CAPRI_FUEL_UPDATE, id=fuel_id, fueldata={'amount': amount, 'price': price})
-#            user.bundle(consts.CAPRI_FUEL).doAction(consts.CAPRI_FUEL_UPLOAD_IMAGE, id=fuel_id, image=form.cleaned_data['image'])
-            # apparently you can't pass an image through protobuf! so we're calling it directly from the front end
-            uploadImage(user, fuel_id, form.cleaned_data['image'])
-            k = FormExecuted()
-            k.data = {
-                    'fuel_id': fuel_id,
-                    'amount': amount,
-                    'price': price,
-                    'image': '/fuel/image/{id:d}/'.format(id=fuel_id),
-                    }
-            raise k
-
 @csrf_protect
 @requires_liveuser
 def play(request, user):
@@ -196,5 +168,23 @@ def finalprize(request, user):
     return render_to_response(
             'fuel/finalprize.html',
             {'user': user, 'consts':consts},
+            context_instance = RequestContext(request)
+            )
+
+
+
+@requires_liveuser
+def history(request, user):
+    return render_to_response(
+            'fuel/history.html',
+            {'user':user, 'consts':consts},
+            context_instance = RequestContext(request)
+            )
+
+@requires_liveuser
+def faq(request, user):
+    return render_to_response(
+            'fuel/faq.html',
+            {'user':user, 'consts':consts},
             context_instance = RequestContext(request)
             )
